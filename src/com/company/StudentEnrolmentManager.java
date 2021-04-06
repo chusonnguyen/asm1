ecdd53078bc906de2d5f969446e48a81e6f9c5de
chusonnguyen <s3742891@rmit.edu.vn>
2021-04-06 15:27:02 +0700

    add Interface

---
+++ src/com/company/StudentEnrolmentManager.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public interface StudentEnrolmentManager {
+    void add() throws InterruptedException;
+    void update(String studentID, String semester) throws InterruptedException;
+    void delete(StudentEnrolment enrolment) throws InterruptedException;
+    void getOne();
+    void getOne(String courseID, String semester);
+    void getAll();
+}
